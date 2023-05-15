cac1a648ae96b6f7819d5646194293589cffe412
Anurag Srivastav <anuragsrivastav0027@gmail.com>
2023-05-15 23:29:34 +0530

    v-1.0.1 updated marquee text

--- src/components/notification.jsx
+++ src/components/notification.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Notification = () => {
   return (
-    <div className=' w-[100%] mt-5 bg-red-500 text-2xl text-white'><marquee width="100%" direction="left" height="30px">
-    Projects are hoisted on free platforms so there are chances that backend api's might be inactive sometimes.
+    <div className=' w-[80%] sm:w-[100%] mx-auto mt-5 bg-red-500 text-2xl text-white'><marquee width="100%"  direction="left" height="30px">
+    Note! Project Api's are hoisted on free tier platforms so there are chances that api's might be inactive sometimes.
     </marquee></div>
   )
 }
